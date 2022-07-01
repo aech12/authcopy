3216307d6b3c8fd7a3d127d1bd8eabff55c7902f
aech12 <aech-12@hotmail.com>
2022-06-30 21:51:08 -0300

    trying to post to insta API

--- src/pages/Insta.jsx
+++ src/pages/Insta.jsx
@@ -7,7 +7,9 @@ const Insta = () => {
 	// console.log(location.search.substring(6), "userId",);
 
   const getUrl = async () => {
-    const data = await axios.post("https://api.instagram.com/oauth/access_token%20/%20client_id=585678779606719/%20client_secret=f7cf10e31240bb35f46b2808da723949/%20grant_type=authorization_code%20/%20redirect_uri=https://authcopy.vercel.app/insta%20/code=AQDTGZ93Hu_q0AyEFK3RbzKzGcn_SiTnsg0JYVVznvB46PJ29Q4ZtMd1N04LMd7UAwAHADRQuIFUfUCPCwt_bHV1G2h9NcgC_zWqhF3q3z7Qdjtvm6itu9yfmgXsJc0sOD0WruOGmaQiWZLDN-IFPDNcAbSao4xbAdfd2dusTyhWzRxKWIJ2Fn2zzff_WLSbMOxNJHy74UFu68ft3jXEsEADY1hSlkApcC0h4g5M-xMXbA#_")
+    // const data = await axios.get("http://localhost:5000/insta/get")
+    const data = await axios.post("http://localhost:5000/insta/get-token")
+    // const data = await axios.post("https://api.instagram.com/oauth/access_token%20/%20client_id=585678779606719/%20client_secret=f7cf10e31240bb35f46b2808da723949/%20grant_type=authorization_code%20/%20redirect_uri=https://authcopy.vercel.app/insta%20/code=AQDTGZ93Hu_q0AyEFK3RbzKzGcn_SiTnsg0JYVVznvB46PJ29Q4ZtMd1N04LMd7UAwAHADRQuIFUfUCPCwt_bHV1G2h9NcgC_zWqhF3q3z7Qdjtvm6itu9yfmgXsJc0sOD0WruOGmaQiWZLDN-IFPDNcAbSao4xbAdfd2dusTyhWzRxKWIJ2Fn2zzff_WLSbMOxNJHy74UFu68ft3jXEsEADY1hSlkApcC0h4g5M-xMXbA#_")
 
     console.log("DATAA", data)
   }
